687559407e56fdaef2fb5d9ff80aa10bb81e70b8
Abhisharma212 <abhisheksharmafeb100@gmail.com>
2021-08-13 18:47:28 +0530

    Abhi React internship Task-2

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+import web from "../src/images/webdev.JPG";
+import app from "../src/images/app.JPG";
+import software from "../src/images/software.JPG";
+import digital from "../src/images/digital.JPG";
+import marketing from "../src/images/marketing.JPG";
+import android from "../src/images/android.JPG";
+
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "Web development",
+    },
+    
+    {
+        imgsrc: app,
+        title:"App development",
+    },
+    {
+        imgsrc: software,
+        title:"Software development",
+    },
+    {
+        imgsrc: digital,
+        title:"Digital Marketing",
+    },
+    {
+        imgsrc: marketing,
+        title:"Marketing",
+    },
+    {
+        imgsrc: android,
+        title:"Android Application",
+    }
+]
+
+export default Sdata;
+
---
+++ src/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <footer className="w-100 bg-light text-center">
+            <br />
+            <p>
+            © 2021 AbhishekTech. All Rights Reserved | Terms And Condition Applied 
+            </p>
+        </footer>
+        </>
+    );
+};
+
+export default Footer;
